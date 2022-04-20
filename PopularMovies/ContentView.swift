840a6b5a6ef8f4044555572ab44b7ff9ae63df1d
RodasOscar <rodasmontoyaoscarandres@gmail.com>
2022-04-20 14:50:29 +0200

    Initial Commit

---
+++ PopularMovies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopularMovies
+//
+//  Created by Oscar Andrés Rodas Montoya on 20/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Oscar Andrés Rodas Montoya on 20/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
